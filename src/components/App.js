c5ea4bee1653a6fe0a543834c08b50c4ee9f16cd
madhur <madhurpanghal0001@gmail.com>
2025-11-23 23:09:07 +0530

    sec

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
-               <ol key={relativeList}>
-                <li key={relativeListItem1}>Rahul</li>
-                <li key={relativeListItem2}>Aryan</li>
-                <li key={relativeListItem3}>Anil</li>
+               <ol key='relativeList'>
+                <li key='relativeListItem1'>Rahul</li>
+                <li key='relativeListItem2'>Aryan</li>
+                <li key='relativeListItem3'>Anil</li>
                </ol>
     </div>
   )
